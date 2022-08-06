c538c791dc14b50d8cbd8898fe89995e22142d36
vinnidias <vinicius.3991@gmail.com>
2022-08-06 17:42:42 -0300

    create task route created

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CreateTask } from "../pages/CreateTask";
 
 import { Home } from "../pages/Home";
 
-
 export const Router: React.FC = () => {
-  return(
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}>
-        
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/createTask" element={<CreateTask />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
