f3dee2ff11adf7663fe90a44443b0b74c9b078c1
vinnidias <vinicius.3991@gmail.com>
2022-08-07 13:57:48 -0300

    input styles corrected

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -2,21 +2,19 @@ import styled from "styled-components";
 import { Theme } from "../../styles/Theme";
 
 export const Container = styled.input`
-  width: fit-content;
+  width: 100%;
   height: 3em;
-  max-width: 180px;
 
-  padding: 0 .5em;
+  padding: 0 0.5em;
   border-radius: 5px;
-  border: 1px solid ${Theme.colors.secondaryBg};
+  border: 1px solid ${Theme.colors.smoothGray};
   box-shadow: 15px 15px 15px -5px rgba(0, 0, 0, 0.1);
 
-  font-family: 'Poppins', sans-serif;
-  font-weight: 600;
+  font-family: "Poppins", sans-serif;
   font-size: 1em;
-  
-  background-color: ${Theme.colors.smoothGray};
   outline: none;
 
-
-`
+  ::placeholder {
+    color: ${Theme.colors.smoothGray};
+  }
+`;
