be59bb073727ef1b32cf1fa23e7132c891f78446
vinnidias <vinicius.3991@gmail.com>
2022-08-07 14:01:14 -0300

    passing tasks provider to routes

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { CreateTask } from "../pages/CreateTask";
 
+import { TasksProvider } from "../contexts/TasksContext";
+import { CreateTask } from "../pages/CreateTask";
 import { Home } from "../pages/Home";
 
 export const Router: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/createTask" element={<CreateTask />} />
-      </Routes>
+      <TasksProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createTask" element={<CreateTask />} />
+        </Routes>
+      </TasksProvider>
     </BrowserRouter>
   );
 };
